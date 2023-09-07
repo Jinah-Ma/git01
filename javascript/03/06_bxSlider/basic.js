366f999c25667e0aeb44c325ed1438010ca7708e
Jinah-Ma <1618wlsdk@gmail.com>
2023-09-07 16:53:12 +0900

    반응형

--- javascript/03/06_bxSlider/basic.js
+++ javascript/03/06_bxSlider/basic.js
@@ -7,3 +7,51 @@ $('.basic_slider').bxSlider({
     auto : true,
     autoHover : true,
 });
+
+$('.img_ctl_slider').bxSlider({
+    prevSelector : ".img_ctl .prev",
+    nextSelector : ".img_ctl .next",
+    /*  multiple : 슬라이드 이미지 여러개 나오는 것 */
+    minSlides : 1,
+    maxSlides : 4,
+    moveSlides : 1,
+    slideWidth : 200,
+    slideMargin : 30,
+    auto : true,
+    speed : 400,
+});
+
+// 
+
+$('.responsive_slider').bxSlider({
+    minSlides : 1,
+    maxSlides : 4,
+    slideWidth : 800, /* 알아서 비율을 줄여준다. */
+    slideMargin : 30,
+    auto : true,
+});
+
+//
+
+$('.video_slider').bxSlider({
+    video : true,
+    onSliderLoad : function(currentIndex){
+        $('.video_slider li').eq(currentIndex + 1).find('video').get(0).play();
+    },
+    onSliderAfter : function($slideElement){
+        $slideElement.siblings().find('video').get(0).pause();
+        $slideElement.find('video').get(0).play();
+    }
+});
+
+
+//
+
+$('.ticker_slider').bxSlider({
+    minSlides: 4,
+    maxSlides: 4,
+    slideWidth: 170,
+    slideMargin: 10,
+    ticker: true,
+    speed: 9000
+  });
